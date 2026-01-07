a44790f85df2ab0374efc68b255f7f57a98bd046
Leejun14 <777ljy777@gmail.com>
2026-01-07 20:22:24 +0900

    docs: API 명세서 작성

---
+++ src/main/java/com/example/schedulemanagementappdev/ScheduleManagementAppDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulemanagementappdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManagementAppDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleManagementAppDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/schedulemanagementappdev/ScheduleManagementAppDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulemanagementappdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManagementAppDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
